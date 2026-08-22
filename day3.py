a2370ba02bbe056d779ed59d95c8841c74d22173
akuthamraju2118-rgb <akuthamraju2118@gmail.com>
2026-08-22 12:51:28 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,11 @@
+target_text = "16 lpa placement 2027 "
+
+salary = target_text[0:2]
+print("salary:", salary)
+
+word = target_text[7:17]
+print("target word:", word)
+
+year = target_text[-4: ]
+print("graduation year:", year)
+
