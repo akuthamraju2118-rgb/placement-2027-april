54dd3a2837cea8d63e25655d6cd8550a6c781492
akuthamraju2118-rgb <akuthamraju2118@gmail.com>
2026-08-31 11:51:44 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,52 @@
+import random
+'''
+ 1 for rock
+
+-1 for paper
+
+0 for scissors
+
+
+'''
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"r" : 1, "p" : -1, "s" : 0}
+reverseDict = {1 : "rock", -1 : "paper", 0 : "scissors"}
+
+you = youDict[youstr]
+
+print(f"you chose {reverseDict[you]}\ncomputer chose {reverseDict[computer]}")
+
+if (computer == you):
+    print("Its a draw")
+
+else:
+    if(computer == -1 and you == 1):
+        print("you lose!")
+        
+    elif(computer == -1 and you == 0):
+        print("you win!")
+
+    elif(computer == 1 and you == 0):
+        print("you lose!")
+
+    elif(computer == 1 and you == -1):
+        print("you win!")
+
+    elif(computer == 0 and you == -1):
+        print("you lose!")
+
+    elif(computer == 0 and you == 1):
+        print("you win!")
+
+    else:
+        print("Something went wrong")
+
+
+
+
+
+
+
+
